7e3e85ef1725deab8d6ff10b539a8de97350ccb5
VIS000 <vis000dev@gmail.com>
2013-06-04 18:35:36 +0300

    Create index.php
    all the scriptie got :P

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/*
+        ProjTrollAlvaro
+  Enjoy your Failzz Fail0r Alvaro
+*/
+
+//Whait whaa? no OOP script?
+//I can't script OOP so i dont give a fuck (yet)
+
+
+
+//Main variable (Because they ar gangsta)
+$alvaroisgay = 'yes'; //TIP: Don't set this to no
+
+
+
+
+
+//The crappy script begins......
+ if ($alvaroisgay == "yes")
+  {
+	 echo "Yaayy. you're right!!!";
+	 //Now you're my best friend yaay :)
+	}
+	
+ elseif ($alvaroisgay == "no")
+   //Oh crap here it comes :0 (Thats what she said :P)
+	{
+	 echo "Your mommie is a Bobba!! :]";
+	}
+   //Don't mess wit da bess G
+ else
+   //What happends to people tryin to mess me up
+    {
+	  echo $alvaroisgay . ' is not a yes or a no';
+    }
+	
+//Shitty script keeps shitty but i don't giv a fuuuuuuuuck!
+?>
